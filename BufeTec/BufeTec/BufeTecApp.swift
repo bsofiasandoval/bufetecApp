498bfd0163a11be84c5b98bb7ac523f735185982
Brenda Sofia Sandoval <bsofiasandoval2@gmail.com>
2024-09-13 21:39:43 -0700

    arreglos

---
+++ BufeTec/BufeTec/BufeTecApp.swift
@@ -0,0 +1,17 @@
+//
+//  BufeTecApp.swift
+//  BufeTec
+//
+//  Created by Sofia Sandoval on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct BufeTecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
